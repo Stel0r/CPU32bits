beb9975909a321231e3d91238b192a68cc9e0c7c
Diego Gamez <diego.felipe.gamez@gmail.com>
2023-12-04 22:05:44 -0500

    restringido el valor del MAR

--- src/logica/Registro4Bit.java
+++ src/logica/Registro4Bit.java
@@ -14,7 +14,7 @@ public class Registro4Bit implements IRegistro {
     // Carga valor de 4 bits en el registro
     public void setValor(int v) {
         // Asegúrese de que la entrada sea de 4 bits
-        if (v > 0b1111111111111111111111) {
+        if (v > 0b1111111111111111111111 || v > 4000000) {
             throw new RuntimeException();
         }
         this.valor = v;
